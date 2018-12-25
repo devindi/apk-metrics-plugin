723dd4df93c0d7071ff6ce810bfc5af6e43b9868
Andrew Mihin <andrew@devindi.com>
2018-12-25 23:27:43 +0300

    Add dexdeps as library

--- apk-metrics/src/main/groovy/com/devindi/gradle/apm/ApkMetricsPlugin.groovy
+++ apk-metrics/src/main/groovy/com/devindi/gradle/apm/ApkMetricsPlugin.groovy
@@ -2,7 +2,7 @@ package com.devindi.gradle.apm
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.api.BaseVariant
-
+import com.devindi.gradle.apm.dex.DexCountTask
 import com.devindi.gradle.apm.size.ApkSizeTask
 import com.devindi.gradle.internal.AppInfoResolver
 import com.devindi.gradle.internal.MetricsReporter
@@ -13,18 +13,19 @@ import org.gradle.api.DomainObjectSet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+@SuppressWarnings("GrMethodMayBeStatic")
 class ApkMetricsPlugin implements Plugin<Project> {
 
 	void apply(Project project) {
 		project.logger.lifecycle("Hello world, apk metrics.!")
 		if (project.plugins.hasPlugin("com.android.application")) {
 			applyToProject(project, project.extensions.findByType(AppExtension).applicationVariants)
+			initDexCount(project, project.extensions.findByType(AppExtension).applicationVariants)
 		} else {
 			project.logger.lifecycle("Project is not app")
 		}
 	}
 
-	@SuppressWarnings("GrMethodMayBeStatic")
 	private def applyToProject(Project project, DomainObjectSet<BaseVariant> variants) {
 		HttpLoggingInterceptor logging = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
 			@Override
@@ -57,6 +58,22 @@ class ApkMetricsPlugin implements Plugin<Project> {
 				project.logger.lifecycle("Create task $task.name")
 
 				project.logger.lifecycle("flavors $variant.name - ${variant.productFlavors[0].name}")
+			}
+		}
+	}
+
+	private void initDexCount(Project project, DomainObjectSet<BaseVariant> variants) {
+		variants.all { BaseVariant variant ->
+			variant.outputs.each { output ->
+				def key = variant.name.capitalize()
+
+				if (variant.outputs.size() > 1) {
+					key += output.name
+				}
+
+				DexCountTask task = project.tasks.create("dexCount$key", DexCountTask) as DexCountTask
+				task.description = "Collect dex count"
+				task.group = "devindi"
 			}
 		}
 	}
---
+++ apk-metrics/src/main/groovy/com/devindi/gradle/apm/dex/DexCountTask.groovy
@@ -0,0 +1,15 @@
+package com.devindi.gradle.apm.dex
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class DexCountTask extends DefaultTask {
+
+    @SuppressWarnings("unused")
+    @TaskAction
+    void reportDexCount() {
+
+        
+
+    }
+}
